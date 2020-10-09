b2d011c1b4d333b99307e73b6b2368fbc12883fe
ranameenu <meenurana32@gmail.com>
2020-10-09 22:45:41 +0530

    complete project

---
+++ process.php
@@ -0,0 +1,14 @@
+<?php 
+if(isset($_Post['email']))
+{
+
+    // Email Information
+    $email_admin = "meenurana32@gmil.com";
+    $name = $_POST['name'];
+    $email = $_Post['Email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+
+    // send email
+}
